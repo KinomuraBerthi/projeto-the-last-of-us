72a9034990c72aa6e21925a9afe4a077af1cbd96
Kinomura Berthi <129013153+KinomuraBerthi@users.noreply.github.com>
2023-03-26 20:15:57 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,45 @@
+/*
+
+Objetivo - quando clicarmos no botão temos que mostrar a imagem de fundo correspondente
+
+
+*/
+
+
+const botoesCarrosel = document.querySelectorAll('.botao');
+
+const imagens = document.querySelectorAll('.imagem');
+
+botoesCarrosel.forEach((botao, indice) => {
+    botao.addEventListener('click', () => {
+
+        desativarBotaoSelecionado();
+
+        selecionarBotaoCarrosel(botao);
+
+        esconderImagemAtiva();
+
+        mostrarImagemDeFundo(indice);
+
+    })
+})
+
+
+function mostrarImagemDeFundo(indice) {
+    imagens[indice].classList.add('ativa');
+}
+
+function selecionarBotaoCarrosel(botao) {
+    botao.classList.add('selecionado');
+}
+
+function esconderImagemAtiva() {
+    const imagemAtiva = document.querySelector('.ativa');
+    imagemAtiva.classList.remove('ativa');
+}
+
+function desativarBotaoSelecionado() {
+    const botaoSelecionado = document.querySelector('.selecionado');
+    botaoSelecionado.classList.remove('selecionado');
+}
+
